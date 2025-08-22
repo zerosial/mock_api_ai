33b5adce8b048110374a5b941725e2c4fa5536db
Sunghum Paik (Brian) <zerosial@gmail.com>
2025-08-22 10:14:25 +0900

    feat: docker 설정을 위한 사전 baseUrl 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone", // Docker 배포를 위한 설정
+  basePath: "/mockapi",
   serverExternalPackages: ["@prisma/client"],
   eslint: {
     // 빌드 시 ESLint 검사 비활성화 (개발 시에는 여전히 작동)
